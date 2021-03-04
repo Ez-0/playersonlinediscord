a29e28c7d6ec61e8fa50ccecd2004c5513e0cdd2
Ez-0 <72645037+Ez-0@users.noreply.github.com>
2021-03-04 17:25:41 +0100

    Delete DDDDDDDDD.bat

--- DDDDDDDDD.bat
+++
@@ -1,7 +0,0 @@
-echo "# playersonlinediscord" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Ez-0/playersonlinediscord.git
-git push -u origin main
